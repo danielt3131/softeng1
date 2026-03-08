c63072fcbb906fac2379c87629bf96cbe3df36f1
Katherine <katherinemtoro@icloud.com>
2026-03-08 15:52:11 -0400

    Delete Smart Study Planner  Github directory

--- Smart Study Planner  Github/src/main/java/edu/unf/smartplanner/model/TaskStatus.java
+++
@@ -1,7 +0,0 @@
-package edu.unf.smartplanner.model;
-
-import java.io.Serializable;
-
-public enum TaskStatus implements Serializable {
-    NOT_STARTED, IN_PROGRESS, COMPLETED
-}
--- Smart Study Planner  Github/src/main/java/edu/unf/smartplanner/model/TaskType.java
+++
@@ -1,7 +0,0 @@
-package edu.unf.smartplanner.model;
-
-import java.io.Serializable;
-
-public enum TaskType implements Serializable {
-    ASSIGNMENT, QUIZ, EXAM, PROJECT
-}
